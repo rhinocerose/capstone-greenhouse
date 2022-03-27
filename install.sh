85c757ee3e0e2b9e5876281e2ba910290beec632
rhinocerose <ashar.k.latif@gmail.com>
2022-03-27 17:03:58 -0400

    Added install script

--- install.sh
+++ install.sh
@@ -3,11 +3,14 @@ sudo apt update
 sudo apt upgrade
 sudo apt install -y git wget python3 python3-pip
 
+sudo apt-get install gnupg
 wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -
 echo "deb http://repo.mongodb.org/apt/debian buster/mongodb-org/5.0 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
 
 sudo apt update
 sudo apt-get install -y mongodb-org
 sudo systemctl start mongod
+
+ufw allow 8050/tcp
 
 pip install -r requirements.txt
